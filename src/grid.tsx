8e771b3def4d0bbb25d3e1bad20692f7ccceb994
Ned Wilde <b.nwilde@gmail.com>
2023-05-12 12:20:51 -0700

    Remove one atom, simpler code, same error

--- src/grid.tsx
+++ src/grid.tsx
@@ -1,11 +1,10 @@
 import {AgGridReact} from "ag-grid-react";
 import {useAtomValue} from "jotai/index";
-import {stateOne, stateTwo} from "./state";
+import {state} from "./state";
 
 export const Grid = () => {
-    const two = useAtomValue(stateTwo);
-    const one = useAtomValue(stateOne);
-    console.log(one, two);
+    const jotaiState = useAtomValue(state);
+    console.log(jotaiState);
     return <div>
         <div className="ag-theme-alpine" style={{ width: '200px', height: '200px' }}>
             <AgGridReact
--- src/state.tsx
+++ src/state.tsx
@@ -6,18 +6,12 @@ export const getLocationId = atom((get) => {
     return get(locationAtom).searchParams?.get("id");
 }) ;
 
-export const stateOne = atom(async () => {
-    const response = await simulateServer(1000);
-    return response;
-})
-
-export const stateTwo = atom( async (getter) => {
+export const state = atom( async (getter) => {
     const skip = getter(getLocationId);
     if(!skip) return 0;
     const response = await simulateServer(1000);
     return response;
 });
-
 
 const simulateServer = (delay: number): Promise<number> => {
     return new Promise(resolve => setTimeout(() => resolve(Math.random()), delay));
